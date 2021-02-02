5c29cd5e237579d4752fe646b6b191d0c3a442e0
GIOme3c <giome3c@yandex.ru>
2021-02-03 01:40:47 +0300

    DataBase complete

--- webapp/data/admin.py
+++ webapp/data/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Place)
+admin.site.register(Position)
+admin.site.register(MobilePhone)
+admin.site.register(Session)
+admin.site.register(Order)
+admin.site.register(ItemCountType)
+admin.site.register(Item)
+admin.site.register(StartSession)
+admin.site.register(EndSession)
+admin.site.register(AddToSession)
+admin.site.register(DeleteOnSession)
