af8c2d9b9cc68e4c82308d9ce90503b52c6d44d7
GIOme3c <giome3c@yandex.ru>
2021-02-24 22:00:30 +0300

    new pages3

--- webapp/staffapp/urls.py
+++ webapp/staffapp/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.pg_index, name="staffapp"),
     path('settings/', views.pg_settings, name="staffapp-setting"),
+    path('session', views.pg_session, name="session"),
     path('session/add/', views.pg_session_add, name="session-add"),
+    path('session/end/', views.pg_session_end, name="session-end"),
+    path('session/change/', views.pg_session_change, name="session-change"),
+    path('session/init/', views.pg_session_init, name="session-init"),
+    path('session/addorder/', views.pg_session_addorder, name="session-addorder"),
+
 ]
--- webapp/staffapp/views.py
+++ webapp/staffapp/views.py
@@ -9,3 +9,18 @@ def pg_settings(request):
 
 def pg_session_add(request):
     return render(request, 'staffapp/pg_add_session.html')
+
+def pg_session_end(request):
+    return render(request, 'staffapp/pg_end_session.html')
+
+def pg_session_init(request):
+    return render(request, 'staffapp/pg_init_session.html')
+
+def pg_session(request):
+    return render(request, 'staffapp/pg_session.html')
+
+def pg_session_change(request):
+    return render(request, 'staffapp/pg_change_session.html')
+
+def pg_session_addorder(request):
+    return render(request, 'staffapp/pg_add_order.html')
